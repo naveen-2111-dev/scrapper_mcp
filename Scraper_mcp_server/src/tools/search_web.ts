62f61c74cf06d09f18bd0d8923eac96e293e0f26
Rajan-5996 <rajan.naveen@gwcdata.ai>
2026-03-30 17:36:57 +0530

    fix

--- Scraper_mcp_server/src/tools/search_web.ts
+++ Scraper_mcp_server/src/tools/search_web.ts
@@ -1,6 +1,6 @@
 import { config, getJson } from "serpapi";
 
-config.api_key = "a98d2ae1619f539c84a57328d63eb63873f5f4a9d3427a005e45fb3adacb827c";
+config.api_key = "YOUR_SERPAPI_API_KEY";
 
 export async function searchWeb(query: string, location: string) {
     return new Promise<{ organic_results: string[] }>((resolve, reject) => {
